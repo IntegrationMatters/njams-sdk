67aa6d6a5d20ea65699ddd4826756afccf7279fe
cwinkler <cwinkler@integrationmatters.com>
2018-10-25 17:09:54 +0200

    Added JSON utils.

---
+++ njams-sdk/src/main/java/com/im/njams/sdk/utils/JsonUtils.java
@@ -0,0 +1,37 @@
+package com.im.njams.sdk.utils;
+
+import com.im.njams.sdk.common.JsonSerializerFactory;
+
+/**
+ * Utilities for serializing to and parsing from JSON.
+ *
+ * @author cwinkler
+ *
+ */
+public class JsonUtils {
+
+    private JsonUtils() {
+        // static only
+    }
+
+    /**
+     * Parses the given JSON string and initializes a new object of the given type from that JSON.
+     * @param json The JSON string to parse.
+     * @param type The type of the object to be created from the given JSON.
+     * @return New instance of the given type.
+     * @throws Exception If parsing the given JSON into the target type failed.
+     */
+    public static <T> T parse(String json, Class<T> type) throws Exception {
+        return JsonSerializerFactory.getDefaultMapper().readValue(json, type);
+    }
+
+    /**
+     * Serializes the given object to a JSON string-
+     * @param object The object to serialize.
+     * @return JSON string representing the given object.
+     * @throws Exception If serializing the object to JSON failed.
+     */
+    public String serialize(Object object) throws Exception {
+        return JsonSerializerFactory.getDefaultMapper().writeValueAsString(object);
+    }
+}
