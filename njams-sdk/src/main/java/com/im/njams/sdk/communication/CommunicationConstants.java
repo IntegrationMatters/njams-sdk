cd0f1ba43e0b400af3f8ca03e388f13e7bf61652
krautenberg <krautenberg@integrationmatters.com>
2019-05-16 08:36:38 +0200

    [SDk-109] Create CommunicationConstants

---
+++ njams-sdk/src/main/java/com/im/njams/sdk/communication/CommunicationConstants.java
@@ -0,0 +1,29 @@
+package com.im.njams.sdk.communication;
+
+public class CommunicationConstants {
+
+    private CommunicationConstants(){}
+
+
+    public static final String COMMUNICATION = "njams.sdk.communication";
+    /**
+     * Property njams.client.sdk.instantpush
+     */
+    public static final String PROPERTY_INSTANT_PUSH = "njams.client.sdk.instantpush";
+    /**
+     * Property njams.client.sdk.minqueuelength
+     */
+    public static final String PROPERTY_MIN_QUEUE_LENGTH = "njams.client.sdk.minqueuelength";
+    /**
+     * Property njams.client.sdk.maxqueuelength
+     */
+    public static final String PROPERTY_MAX_QUEUE_LENGTH = "njams.client.sdk.maxqueuelength";
+    /**
+     * Property njams.client.sdk.senderidletime
+     */
+    public static final String PROPERTY_SENDER_THREAD_IDLE_TIME = "njams.client.sdk.senderthreadidletime";
+    /**
+     * Property njams.client.sdk.discardpolicy
+     */
+    public static final String PROPERTY_DISCARD_POLICY = "njams.client.sdk.discardpolicy";
+}
