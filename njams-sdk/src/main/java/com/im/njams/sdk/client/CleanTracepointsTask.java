b4324e41cd80d6ea45311d12a0ec0ea8aa3b8707
cwinkler <cwinkler@integrationmatters.com>
2020-03-25 15:54:11 +0100

    SDK-199 SDK-200 Fixed concurrency issues by using ConcurrentHashMaps where iterators should be thread-safe also.

--- njams-sdk/src/main/java/com/im/njams/sdk/client/CleanTracepointsTask.java
+++ njams-sdk/src/main/java/com/im/njams/sdk/client/CleanTracepointsTask.java
@@ -1,14 +1,14 @@
-/* 
+/*
  * Copyright (c) 2019 Faiz & Siegeln Software GmbH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * 
+ *
  * The Software shall be used for Good, not Evil.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
@@ -16,20 +16,27 @@
  */
 package com.im.njams.sdk.client;
 
-import com.faizsiegeln.njams.messageformat.v4.tracemessage.*;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import org.slf4j.LoggerFactory;
+
+import com.faizsiegeln.njams.messageformat.v4.tracemessage.Activity;
+import com.faizsiegeln.njams.messageformat.v4.tracemessage.TraceMessage;
 import com.im.njams.sdk.Njams;
+import com.im.njams.sdk.common.DateTimeUtility;
+import com.im.njams.sdk.common.NjamsSdkRuntimeException;
 import com.im.njams.sdk.configuration.ActivityConfiguration;
 import com.im.njams.sdk.configuration.Configuration;
 import com.im.njams.sdk.configuration.ProcessConfiguration;
 import com.im.njams.sdk.configuration.TracepointExt;
-import com.im.njams.sdk.common.DateTimeUtility;
-import com.im.njams.sdk.common.NjamsSdkRuntimeException;
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-
-import org.slf4j.LoggerFactory;
 
 /**
  * Task for iterating over every Njams instance and check for outdated
@@ -41,7 +48,7 @@ public class CleanTracepointsTask extends TimerTask {
 
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(CleanTracepointsTask.class);
 
-    private static Map<String, Njams> njamsInstances = new HashMap<>();
+    private static Map<String, Njams> njamsInstances = new ConcurrentHashMap<>();
 
     private static Timer timer = null;
 
@@ -75,7 +82,7 @@ public class CleanTracepointsTask extends TimerTask {
      *
      * @return list of njams instances
      */
-    static List<Njams> getNjamsInstances(){
+    static List<Njams> getNjamsInstances() {
         return njamsInstances.values().stream().collect(Collectors.toList());
     }
 
@@ -84,7 +91,7 @@ public class CleanTracepointsTask extends TimerTask {
      *
      * @return timer
      */
-    static Timer getTimer(){
+    static Timer getTimer() {
         return timer;
     }
 
@@ -125,17 +132,19 @@ public class CleanTracepointsTask extends TimerTask {
     private void checkNjams(Njams njams, LocalDateTime now) {
         Configuration configuration = njams.getConfiguration();
         TraceMessageBuilder tmBuilder = new TraceMessageBuilder(njams);
-        configuration.getProcesses().entrySet().forEach(processEntry -> checkProcess(configuration, processEntry, now, tmBuilder));
+        configuration.getProcesses().entrySet()
+                .forEach(processEntry -> checkProcess(configuration, processEntry, now, tmBuilder));
         TraceMessage msg = tmBuilder.build();
-        if(msg != null){
+        if (msg != null) {
             njams.getSender().send(msg);
         }
     }
 
-    private void checkProcess(Configuration configuration, Entry<String, ProcessConfiguration> processEntry, LocalDateTime now, TraceMessageBuilder tmBuilder) {
+    private void checkProcess(Configuration configuration, Entry<String, ProcessConfiguration> processEntry,
+            LocalDateTime now, TraceMessageBuilder tmBuilder) {
         //use itertor here, because we want to possibly modify the map itself
-        ProcessModel model = new ProcessModel();
-        Iterator<Entry<String, ActivityConfiguration>> it = processEntry.getValue().getActivities().entrySet().iterator();
+        Iterator<Entry<String, ActivityConfiguration>> it =
+                processEntry.getValue().getActivities().entrySet().iterator();
         while (it.hasNext()) {
             Entry<String, ActivityConfiguration> ae = it.next();
             if (checkActivity(configuration, processEntry, ae, now, tmBuilder)) {
@@ -145,7 +154,8 @@ public class CleanTracepointsTask extends TimerTask {
         }
     }
 
-    private boolean checkActivity(Configuration configuration, Entry<String, ProcessConfiguration> processEntry, Entry<String, ActivityConfiguration> ae, LocalDateTime now, TraceMessageBuilder tmBuilder) {
+    private boolean checkActivity(Configuration configuration, Entry<String, ProcessConfiguration> processEntry,
+            Entry<String, ActivityConfiguration> ae, LocalDateTime now, TraceMessageBuilder tmBuilder) {
         ActivityConfiguration activity = ae.getValue();
         TracepointExt tracepoint = activity.getTracepoint();
         if (tracepoint != null && (tracepoint.getEndtime().isBefore(now) || tracepoint.iterationsExceeded())) {
@@ -163,7 +173,8 @@ public class CleanTracepointsTask extends TimerTask {
         return false;
     }
 
-    private void fillTraceMessageBuilder(Entry<String, ProcessConfiguration> processEntry, Entry<String, ActivityConfiguration> ae, TraceMessageBuilder tmBuilder){
+    private void fillTraceMessageBuilder(Entry<String, ProcessConfiguration> processEntry,
+            Entry<String, ActivityConfiguration> ae, TraceMessageBuilder tmBuilder) {
         Activity act = new Activity();
         act.setActivityId(ae.getKey());
         act.setTracepoint(ae.getValue().getTracepoint());
--- njams-sdk/src/main/java/com/im/njams/sdk/configuration/Configuration.java
+++ njams-sdk/src/main/java/com/im/njams/sdk/configuration/Configuration.java
@@ -1,14 +1,14 @@
-/* 
+/*
  * Copyright (c) 2018 Faiz & Siegeln Software GmbH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * 
+ *
  * The Software shall be used for Good, not Evil.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
@@ -16,12 +16,13 @@
  */
 package com.im.njams.sdk.configuration;
 
-import com.faizsiegeln.njams.messageformat.v4.projectmessage.LogMode;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.faizsiegeln.njams.messageformat.v4.projectmessage.LogMode;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * The configuration contains all configuration of the client, which are not part of the
@@ -36,7 +37,7 @@ public class Configuration {
     private ConfigurationProvider configurationProvider;
 
     private LogMode logMode = LogMode.COMPLETE;
-    private Map<String, ProcessConfiguration> processes = new HashMap<>();
+    private Map<String, ProcessConfiguration> processes = new ConcurrentHashMap<>();
 
     /**
      * Should be provided by the Settings
