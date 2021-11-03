81f5923072d16c50fc0f763d551682358ad0d442
Christian Winkler <cwinkler@integrationmatters.com>
2021-11-03 08:22:55 +0100

    Fixed test

--- njams-sdk/src/test/java/com/im/njams/sdk/NjamsTest.java
+++ njams-sdk/src/test/java/com/im/njams/sdk/NjamsTest.java
@@ -1,14 +1,14 @@
-/* 
+/*
  * Copyright (c) 2018 Faiz & Siegeln Software GmbH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * 
+ *
  * The Software shall be used for Good, not Evil.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
@@ -16,24 +16,36 @@
  */
 package com.im.njams.sdk;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import com.faizsiegeln.njams.messageformat.v4.command.Command;
 import com.faizsiegeln.njams.messageformat.v4.command.Instruction;
 import com.faizsiegeln.njams.messageformat.v4.command.Request;
 import com.faizsiegeln.njams.messageformat.v4.command.Response;
 import com.im.njams.sdk.common.NjamsSdkRuntimeException;
 import com.im.njams.sdk.common.Path;
-import com.im.njams.sdk.communication.*;
+import com.im.njams.sdk.communication.CommunicationFactory;
+import com.im.njams.sdk.communication.ReplayHandler;
+import com.im.njams.sdk.communication.ReplayRequest;
+import com.im.njams.sdk.communication.ReplayResponse;
+import com.im.njams.sdk.communication.TestSender;
 import com.im.njams.sdk.logmessage.DataMasking;
 import com.im.njams.sdk.logmessage.Job;
 import com.im.njams.sdk.model.ProcessModel;
-import com.im.njams.sdk.settings.Settings;
 import com.im.njams.sdk.serializer.Serializer;
-
-import java.util.*;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Before;
+import com.im.njams.sdk.settings.Settings;
 
 /**
  *
@@ -104,7 +116,7 @@ public class NjamsTest {
         instance.onInstruction(inst);
         Response resp = inst.getResponse();
         assertTrue(resp.getResultCode() == 0);
-        assertEquals("Successfully send ProjectMessage via NjamsClient", resp.getResultMessage());
+        assertEquals("Successfully sent ProjectMessage via NjamsClient", resp.getResultMessage());
     }
 
     @Test
@@ -141,7 +153,7 @@ public class NjamsTest {
         assertTrue(resp.getResultCode() == 0);
         assertEquals("TestWorked", resp.getResultMessage());
     }
-    
+
     @Test
     public void testOnThrownExceptionReplayMessageInstruction() {
         Instruction inst = new Instruction();
@@ -178,7 +190,7 @@ public class NjamsTest {
     }
 
     @Test
-    public void setDataMaskingViaSettings(){
+    public void setDataMaskingViaSettings() {
         DataMasking.removePatterns();
 
         Settings settings = new Settings();
@@ -193,7 +205,7 @@ public class NjamsTest {
     }
 
     @Test
-    public void disableDataMaskingViaSettings(){
+    public void disableDataMaskingViaSettings() {
         DataMasking.removePatterns();
         Settings settings = new Settings();
         settings.put(DataMasking.DATA_MASKING_ENABLED, "false");
@@ -207,7 +219,7 @@ public class NjamsTest {
     }
 
     @Test
-    public void disableDataMaskingDisablesAllDataMasking(){
+    public void disableDataMaskingDisablesAllDataMasking() {
         DataMasking.removePatterns();
 
         Settings settings = new Settings();
@@ -226,7 +238,7 @@ public class NjamsTest {
     }
 
     @Test
-    public void enableDataMaskingWithoutRegex(){
+    public void enableDataMaskingWithoutRegex() {
         DataMasking.removePatterns();
         Settings settings = new Settings();
         settings.put(DataMasking.DATA_MASKING_ENABLED, "true");
