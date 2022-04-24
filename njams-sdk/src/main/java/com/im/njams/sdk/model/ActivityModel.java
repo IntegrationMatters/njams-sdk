e5d821add21d605f22b03f6d8af5bddd114dcd09
krautenberg <krautenberg@integrationmatters.com>
2022-04-24 18:42:54 +0200

    model refactored

--- njams-sdk/src/main/java/com/im/njams/sdk/model/ActivityModel.java
+++ njams-sdk/src/main/java/com/im/njams/sdk/model/ActivityModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Faiz & Siegeln Software GmbH
+ * Copyright (c) 2022 Faiz & Siegeln Software GmbH
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -22,7 +22,6 @@ import com.im.njams.sdk.common.IdUtil;
 import com.im.njams.sdk.common.NjamsSdkRuntimeException;
 import com.im.njams.sdk.configuration.ActivityConfiguration;
 import com.im.njams.sdk.configuration.ProcessConfiguration;
-import com.im.njams.sdk.settings.Settings;
 
 import javax.xml.bind.annotation.XmlTransient;
 import java.util.*;
@@ -30,8 +29,6 @@ import java.util.stream.Collectors;
 
 /**
  * This class represents a Activity when creating the initial Model.
- *
- * @author stkniep
  */
 public class ActivityModel {
 
