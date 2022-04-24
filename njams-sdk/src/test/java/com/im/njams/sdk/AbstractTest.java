3c61b68bc93afd72d2ef71ba56f167bcbd1a7706
krautenberg <krautenberg@integrationmatters.com>
2022-04-24 19:44:36 +0200

    Refactored all tests

--- njams-sdk/src/test/java/com/im/njams/sdk/AbstractTest.java
+++ njams-sdk/src/test/java/com/im/njams/sdk/AbstractTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Faiz & Siegeln Software GmbH
+ * Copyright (c) 2020 Faiz & Siegeln Software GmbH
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -33,9 +33,6 @@ import java.util.Map;
 
 /**
  * This class is a helper class for all test classes that need some jobs or activities.
- *
- * @author krautenberg@integrationmatters.com
- * @version 4.0.6
  */
 public abstract class AbstractTest {
 
